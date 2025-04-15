21f5dbb7fb1b9dec988947bff1e5ad9219cf4c10
chrisokojie <obiluventures@gmail.com>
2025-04-15 20:57:28 +0100

    Update EC_IT143_W5.2_MyFC_co.sql
    Adding deliverable 5.2.

--- EC_IT143_W5.2_MyFC_co.sql
+++ EC_IT143_W5.2_MyFC_co.sql
@@ -11,34 +11,40 @@ Purpose: This script answers 4 analytical questions about the MyFC community
 
 -- Question 1: Which players had the highest total salary over the season?
 -- Author: Christopher Okojie
-SELECT PlayerName, SUM(Salary) AS TotalSeasonSalary
-FROM PlayerContracts
-GROUP BY PlayerName
+SELECT pl_id, SUM(mtd_salary) AS TotalSeasonSalary
+FROM tblPlayerFact
+GROUP BY pl_id
 ORDER BY TotalSeasonSalary DESC;
 
 -- Question 2: Which teams are spending the most on players based on total salaries?
 -- Author: Christopher Okojie
-SELECT TeamName, SUM(Salary) AS TotalTeamSalary
-FROM PlayerContracts
-GROUP BY TeamName
+SELECT td.t_code, SUM(pf.mtd_salary) AS TotalTeamSalary
+FROM tblPlayerFact pf
+JOIN tblPlayerDim pd ON pf.pl_id = pd.pl_id
+JOIN tblTeamDim td ON pd.t_id = td.t_id
+GROUP BY td.t_code
 ORDER BY TotalTeamSalary DESC;
+
 
 -- Question 3: Which player scored the most points?
 -- Author: Another student
-SELECT PlayerName, SUM(PointsScored) AS TotalPoints
+SELECT TOP 1 pl_name, SUM(PointsScored) AS TotalPoints
 FROM MatchStats
-GROUP BY PlayerName
-ORDER BY TotalPoints DESC
-FETCH FIRST 1 ROWS ONLY;  -- Use TOP 1 if using SQL Server
+GROUP BY pl_name
+ORDER BY TotalPoints DESC;
 
--- Question 4: How many players earn below the team’s average salary?
+
+-- Question 4: How many players earn below the teamâ€™s average salary?
 -- Author: Christopher Okojie
 SELECT COUNT(*) AS PlayersBelowAverage
-FROM PlayerContracts pc
+FROM tblPlayerFact pf
+JOIN tblPlayerDim pd ON pf.pl_id = pd.pl_id
+JOIN tblTeamDim td ON pd.t_id = td.t_id
 JOIN (
-    SELECT TeamName, AVG(Salary) AS TeamAvgSalary
-    FROM PlayerContracts
-    GROUP BY TeamName
-) team_avg
-ON pc.TeamName = team_avg.TeamName
-WHERE pc.Salary < team_avg.TeamAvgSalary;
+    SELECT td_inner.t_code, AVG(pf_inner.mtd_salary) AS TeamAvgSalary
+    FROM tblPlayerFact pf_inner
+    JOIN tblPlayerDim pd_inner ON pf_inner.pl_id = pd_inner.pl_id
+    JOIN tblTeamDim td_inner ON pd_inner.t_id = td_inner.t_id
+    GROUP BY td_inner.t_code
+) team_avg ON td.t_code = team_avg.t_code
+WHERE pf.mtd_salary < team_avg.TeamAvgSalary;
